e2991547fbcebf4c87ce794dc4d613824214a9ed
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-05-19 02:24:55 -0700

    ecommerce-iti update home page

--- js/GurdRoutes.js
+++ js/GurdRoutes.js
@@ -9,6 +9,7 @@ if (sessionStorage.currentUser) {
     console.log("hi",sessionStorage.currentUser);
     var profile = document.querySelector(".current_user");
     profile.textContent=sessionStorage.currentUser
+    document.body.style.display = "block";
 // console.log(d);
     
 }else{
